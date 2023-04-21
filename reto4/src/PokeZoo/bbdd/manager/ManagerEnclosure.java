0364ee16f34e2dbe888a58a2f929931caf2c2e69
in1dw3 <in1dw3@HZ322028.elorrieta.int>
2023-04-21 21:09:52 +0200

    gestor de food y enclosure

--- reto4/src/PokeZoo/bbdd/manager/ManagerEnclosure.java
+++ reto4/src/PokeZoo/bbdd/manager/ManagerEnclosure.java
@@ -1,18 +1,79 @@
 package PokeZoo.bbdd.manager;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.security.auth.login.AccountNotFoundException;
 
 import PokeZoo.bbdd.pojo.Enclosure;
+import PokeZoo.bbdd.utils.DBUtils;
 
 public class ManagerEnclosure implements managerGeneral<Enclosure>{
 
 	@Override
 	public List<Enclosure> selectAll() throws SQLException, AccountNotFoundException, Exception {
-		// TODO Auto-generated method stub
-		return null;
+		ArrayList<Enclosure> ret = null;
+
+		String sql = "select * from Enclosure";
+
+		Connection connection = null;
+		Statement statement = null;
+		ResultSet resultSet = null;
+
+		try {
+			Class.forName(DBUtils.DRIVER);
+
+			connection = DriverManager.getConnection(DBUtils.URL, DBUtils.USER, DBUtils.PASS);
+
+			statement = connection.createStatement();
+			resultSet = statement.executeQuery(sql);
+
+			while (resultSet.next()) {
+				if (null == ret) {
+					ret = new ArrayList<Enclosure>();
+				}
+
+				Enclosure enclo = new Enclosure();
+
+				// añadir datos del Pokemon aqui
+				enclo.setIdEnclosure(resultSet.getInt("idEnclosure"));
+				enclo.setTypeEn(resultSet.getString("typeEn"));
+				enclo.setNumberEn(resultSet.getInt("numberEn"));
+				
+				ret.add(enclo);
+			}
+		} catch (SQLException sqle) {
+			System.out.println("Error con la BBDD - " + sqle.getMessage());
+		} catch (Exception e) {
+			System.out.println("Error generico - " + e.getMessage());
+		} finally {
+
+			try {
+				if (resultSet != null)
+					resultSet.close();
+			} catch (Exception e) {
+
+			}
+			try {
+				if (statement != null)
+					statement.close();
+			} catch (Exception e) {
+
+			}
+			try {
+				if (connection != null)
+					connection.close();
+			} catch (Exception e) {
+
+			}
+		}
+		return ret;
+		
 	}
 
 	@Override
--- reto4/src/PokeZoo/bbdd/manager/ManagerFood.java
+++ reto4/src/PokeZoo/bbdd/manager/ManagerFood.java
@@ -1,18 +1,80 @@
 package PokeZoo.bbdd.manager;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.security.auth.login.AccountNotFoundException;
 
 import PokeZoo.bbdd.pojo.Food;
+import PokeZoo.bbdd.utils.DBUtils;
 
 public class ManagerFood implements managerGeneral<Food>{
 
 	@Override
 	public List<Food> selectAll() throws SQLException, AccountNotFoundException, Exception {
-		// TODO Auto-generated method stub
-		return null;
+		ArrayList<Food> ret = null;
+
+		String sql = "select * from Food";
+
+		Connection connection = null;
+		Statement statement = null;
+		ResultSet resultSet = null;
+
+		try {
+			Class.forName(DBUtils.DRIVER);
+
+			connection = DriverManager.getConnection(DBUtils.URL, DBUtils.USER, DBUtils.PASS);
+
+			statement = connection.createStatement();
+			resultSet = statement.executeQuery(sql);
+
+			while (resultSet.next()) {
+				if (null == ret) {
+					ret = new ArrayList<Food>();
+				}
+
+				Food food = new Food();
+
+				// añadir datos del Pokemon aqui
+				food.setIdFood(resultSet.getInt("idEnclosure"));
+				food.setQuantity(resultSet.getInt("typeEn"));
+				food.setDailyConsume(resultSet.getInt("numberEn"));
+				food.setNameFo(resultSet.getString("nameFo"));
+				food.setDescriptionFo(resultSet.getString("descriptionFo"));
+				
+				ret.add(food);
+			}
+		} catch (SQLException sqle) {
+			System.out.println("Error con la BBDD - " + sqle.getMessage());
+		} catch (Exception e) {
+			System.out.println("Error generico - " + e.getMessage());
+		} finally {
+
+			try {
+				if (resultSet != null)
+					resultSet.close();
+			} catch (Exception e) {
+
+			}
+			try {
+				if (statement != null)
+					statement.close();
+			} catch (Exception e) {
+
+			}
+			try {
+				if (connection != null)
+					connection.close();
+			} catch (Exception e) {
+
+			}
+		}
+		return ret;
 	}
 
 	@Override
