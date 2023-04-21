b0d9e6902fd5f8c3c56a54592cb73c8e5f4d5c5f
in1dw3 <in1dw3@HZ352027.elorrieta.int>
2023-04-21 20:28:23 +0200

    Manager y POJOS

---
+++ reto4/src/PokeZoo/bbdd/pojo/Pokemon.java
@@ -0,0 +1,9 @@
+package PokeZoo.bbdd.pojo;
+
+import java.io.Serializable;
+
+public class Pokemon implements Serializable{
+
+	private static final long serialVersionUID = 4268344699422278647L;
+
+}
