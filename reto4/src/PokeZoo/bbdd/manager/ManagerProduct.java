57cf65154f60db5834a5f4be5d3825f128be42dc
in1dw3 <in1dw3@HZ322028.elorrieta.int>
2023-04-21 21:20:46 +0200

    Manager Product sql hecho + POJO de Product

--- reto4/src/PokeZoo/bbdd/manager/ManagerProduct.java
+++ reto4/src/PokeZoo/bbdd/manager/ManagerProduct.java
@@ -1,36 +1,97 @@
 package PokeZoo.bbdd.manager;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.security.auth.login.AccountNotFoundException;
 
 import PokeZoo.bbdd.pojo.Product;
+import PokeZoo.bbdd.utils.DBUtils;
 
-public class ManagerProduct implements managerGeneral<Product>{
+public class ManagerProduct implements managerGeneral<Product> {
 
 	@Override
 	public List<Product> selectAll() throws SQLException, AccountNotFoundException, Exception {
-		// TODO Auto-generated method stub
-		return null;
+		ArrayList<Product> ret = null;
+
+		String sql = "select * from Product";
+
+		Connection connection = null;
+		Statement statement = null;
+		ResultSet resultSet = null;
+
+		try {
+			Class.forName(DBUtils.DRIVER);
+
+			connection = DriverManager.getConnection(DBUtils.URL, DBUtils.USER, DBUtils.PASS);
+
+			statement = connection.createStatement();
+			resultSet = statement.executeQuery(sql);
+
+			while (resultSet.next()) {
+				if (null == ret) {
+					ret = new ArrayList<Product>();
+				}
+
+				Product product = new Product();
+
+				// añadir datos del Pokemon aqui
+				product.setIdProduct(resultSet.getInt("idProduct"));
+				product.setNamePr(resultSet.getString("namePr"));
+				product.setDescriptionPr(resultSet.getString("descripcionPr"));
+				product.setValuePr(resultSet.getDouble("valuePr"));
+
+				ret.add(product);
+			}
+		} catch (SQLException sqle) {
+			System.out.println("Error con la BBDD - " + sqle.getMessage());
+		} catch (Exception e) {
+			System.out.println("Error generico - " + e.getMessage());
+		} finally {
+
+			try {
+				if (resultSet != null)
+					resultSet.close();
+			} catch (Exception e) {
+
+			}
+			try {
+				if (statement != null)
+					statement.close();
+			} catch (Exception e) {
+
+			}
+			try {
+				if (connection != null)
+					connection.close();
+			} catch (Exception e) {
+
+			}
+		}
+		return ret;
 	}
 
 	@Override
 	public void insert(Product t) throws SQLException, Exception {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void update(Product t) throws SQLException, Exception {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void delete(Product t) throws SQLException, Exception {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 }
--- reto4/src/PokeZoo/bbdd/pojo/Product.java
+++ reto4/src/PokeZoo/bbdd/pojo/Product.java
@@ -1,16 +1,90 @@
 package PokeZoo.bbdd.pojo;
 
 import java.io.Serializable;
+import java.util.Objects;
 
-public class Product implements Serializable{
+public class Product implements Serializable {
 
 	private static final long serialVersionUID = 237001453545700048L;
-	
+
+	// primary key
+	private int idProduct = 0;
+
 	// attributes
-	private int idProduct = 0; 
 	private String namePr = "";
 	private String descriptionPr = "";
 	private double valuePr = 0;
+
+	// link Product with shop
 	private Shop shop = null;
-	
+
+	public int getIdProduct() {
+		return idProduct;
+	}
+
+	public void setIdProduct(int idProduct) {
+		this.idProduct = idProduct;
+	}
+
+	public String getNamePr() {
+		return namePr;
+	}
+
+	public void setNamePr(String namePr) {
+		this.namePr = namePr;
+	}
+
+	public String getDescriptionPr() {
+		return descriptionPr;
+	}
+
+	public void setDescriptionPr(String descriptionPr) {
+		this.descriptionPr = descriptionPr;
+	}
+
+	public double getValuePr() {
+		return valuePr;
+	}
+
+	public void setValuePr(double valuePr) {
+		this.valuePr = valuePr;
+	}
+
+	public Shop getShop() {
+		return shop;
+	}
+
+	public void setShop(Shop shop) {
+		this.shop = shop;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(descriptionPr, idProduct, namePr, shop, valuePr);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Product other = (Product) obj;
+		return Objects.equals(descriptionPr, other.descriptionPr) && idProduct == other.idProduct
+				&& Objects.equals(namePr, other.namePr) && Objects.equals(shop, other.shop)
+				&& Double.doubleToLongBits(valuePr) == Double.doubleToLongBits(other.valuePr);
+	}
+
+	@Override
+	public String toString() {
+		return "Product [idProduct=" + idProduct + ", namePr=" + namePr + ", descriptionPr=" + descriptionPr
+				+ ", valuePr=" + valuePr + ", shop=" + shop + "]";
+	}
+
 }
