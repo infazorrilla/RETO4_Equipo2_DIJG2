c0b5d03aea3ec47f00a86218f25a4ba6e6e6684c
in1dw3 <in1dw3@HZ352027.elorrieta.int>
2023-04-21 16:51:59 +0200

    prueba creacion proyecto

---
+++ reto4/src/PokeZoo/bbdd/pojo/Product.java
@@ -0,0 +1,9 @@
+package PokeZoo.bbdd.pojo;
+
+import java.io.Serializable;
+
+public class Product implements Serializable{
+
+	private static final long serialVersionUID = 237001453545700048L;
+	
+}
