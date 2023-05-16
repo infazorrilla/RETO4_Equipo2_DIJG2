da7853ab3477a8692cf89c01e57866166fbcb378
in1dw3 <in1dw3@HZ352029.elorrieta.int>
2023-05-16 16:05:25 +0200

    apañao inicio

--- reto4/src/PokeZoo/PokeZoo.java
+++ reto4/src/PokeZoo/PokeZoo.java
@@ -8,8 +8,7 @@ public class PokeZoo {
 	 * Launch the Poke-Zoo application.
 	 */
 	public static void main(String[] args) {
-		@SuppressWarnings("unused")
-		Views window = new Views();
+		new Views();
 	}
 	
 }
